42f78639d567a2ac5c058e531c0ba1ca1d2c41e3
Brandon <bleventh@gmail.com>
2013-10-19 21:37:17 -0700

    Add view controller class to entry point

---
+++ lrn/EntryPointViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryPointViewController.h
+//  lrn
+//
+//  Created by Brandon Leventhal on 10/19/13.
+//  Copyright (c) 2013 KBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryPointViewController : UIViewController
+
+@end
